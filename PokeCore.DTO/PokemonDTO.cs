4a89729a7ff3f39049093896276eaa4a83e002f4
Milone <milonegabriel000@gmail.com>
2025-10-20 16:08:00 -0300

    feat(DTO): ajusta propriedades e adiciona novos campos

--- PokeCore.DTO/PokemonDTO.cs
+++ PokeCore.DTO/PokemonDTO.cs
@@ -6,8 +6,11 @@
         public string Nome { get; set; }
         public string Tipo { get; set; }
         public int Nivel { get; set; }
-        public int OwnerId { get; set; }
+        public int? OwnerId { get; set; }
         public DateTime CapturedAt { get; set; }
+        public string LocalDeCaptura { get; set; }
         public string Nickname { get; set; }
+        public bool isInActiveTeam { get; set; }
+
     }
 }
