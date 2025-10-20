ef76b262d3639a4310986faf3191466447949ccc
Milone <milonegabriel000@gmail.com>
2025-10-20 16:08:36 -0300

    feat(DTO): ajusta propriedades de DTO

--- PokeCore.DTO/TreinadorDTO.cs
+++ PokeCore.DTO/TreinadorDTO.cs
@@ -5,10 +5,10 @@
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
         public bool IsAdmin { get; set; }
-        public bool IsActive { get; set; }
         public string DisplayName { get; set; }
         public DateTime CreatedAt { get; set; }
+        public string FotoPath { get; set; }
     }
 }
