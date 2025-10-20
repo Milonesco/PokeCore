eb486c7a867932ebf79e2dca2e0690e410d3b721
Milone <milonegabriel000@gmail.com>
2025-10-20 16:03:43 -0300

    feat(usuario): adiciona dashboard com dados e estatísticas

---
+++ PokeCore/ucHome.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace PokeCore.DesktopUI
+{
+    partial class ucHome
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            lblBoasVindas = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lblDataCriacao = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            panelTimeAtivo = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            pbTime6 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            pbTime5 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            pbTime4 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            pbTime3 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            pbTime2 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            pbTime1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            pbUltimoPokemon = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            lblUltimoPokemon = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lblPokemonCapturados = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lblPcBox = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lblNomePokemon = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            panelAdminStats = new Guna.UI2.WinForms.Guna2Panel();
+            lblAdminTotalTreinadores = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lblAdminNovosTreinadores24h = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lblAdminTotalPokemon = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lblAdminNovosPokemons24h = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            panelTimeAtivo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pbTime6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbUltimoPokemon).BeginInit();
+            panelAdminStats.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblBoasVindas
+            // 
+            lblBoasVindas.BackColor = Color.Transparent;
+            lblBoasVindas.Font = new Font("Segoe UI", 32F);
+            lblBoasVindas.Location = new Point(13, 13);
+            lblBoasVindas.Name = "lblBoasVindas";
+            lblBoasVindas.Size = new Size(293, 61);
+            lblBoasVindas.TabIndex = 0;
+            lblBoasVindas.Text = "Seja bem vindo";
+            // 
+            // lblDataCriacao
+            // 
+            lblDataCriacao.BackColor = Color.Transparent;
+            lblDataCriacao.Location = new Point(963, 625);
+            lblDataCriacao.Name = "lblDataCriacao";
+            lblDataCriacao.Size = new Size(89, 17);
+            lblDataCriacao.TabIndex = 1;
+            lblDataCriacao.Text = "Data de Criação:";
+            // 
+            // panelTimeAtivo
+            // 
+            panelTimeAtivo.BackColor = Color.Transparent;
+            panelTimeAtivo.Controls.Add(pbTime6);
+            panelTimeAtivo.Controls.Add(pbTime5);
+            panelTimeAtivo.Controls.Add(pbTime4);
+            panelTimeAtivo.Controls.Add(pbTime3);
+            panelTimeAtivo.Controls.Add(pbTime2);
+            panelTimeAtivo.Controls.Add(pbTime1);
+            panelTimeAtivo.FillColor = Color.White;
+            panelTimeAtivo.Location = new Point(3, 478);
+            panelTimeAtivo.Name = "panelTimeAtivo";
+            panelTimeAtivo.ShadowColor = Color.Black;
+            panelTimeAtivo.Size = new Size(654, 164);
+            panelTimeAtivo.TabIndex = 2;
+            // 
+            // pbTime6
+            // 
+            pbTime6.FillColor = SystemColors.WindowFrame;
+            pbTime6.ImageRotate = 0F;
+            pbTime6.Location = new Point(542, 13);
+            pbTime6.Name = "pbTime6";
+            pbTime6.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            pbTime6.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            pbTime6.Size = new Size(100, 90);
+            pbTime6.TabIndex = 0;
+            pbTime6.TabStop = false;
+            // 
+            // pbTime5
+            // 
+            pbTime5.FillColor = SystemColors.WindowFrame;
+            pbTime5.ImageRotate = 0F;
+            pbTime5.Location = new Point(436, 13);
+            pbTime5.Name = "pbTime5";
+            pbTime5.ShadowDecoration.CustomizableEdges = customizableEdges11;
+            pbTime5.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            pbTime5.Size = new Size(100, 90);
+            pbTime5.TabIndex = 0;
+            pbTime5.TabStop = false;
+            // 
+            // pbTime4
+            // 
+            pbTime4.FillColor = SystemColors.WindowFrame;
+            pbTime4.ImageRotate = 0F;
+            pbTime4.Location = new Point(330, 13);
+            pbTime4.Name = "pbTime4";
+            pbTime4.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            pbTime4.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            pbTime4.Size = new Size(100, 90);
+            pbTime4.TabIndex = 0;
+            pbTime4.TabStop = false;
+            // 
+            // pbTime3
+            // 
+            pbTime3.FillColor = SystemColors.WindowFrame;
+            pbTime3.ImageRotate = 0F;
+            pbTime3.Location = new Point(224, 13);
+            pbTime3.Name = "pbTime3";
+            pbTime3.ShadowDecoration.CustomizableEdges = customizableEdges13;
+            pbTime3.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            pbTime3.Size = new Size(100, 90);
+            pbTime3.TabIndex = 0;
+            pbTime3.TabStop = false;
+            // 
+            // pbTime2
+            // 
+            pbTime2.FillColor = SystemColors.WindowFrame;
+            pbTime2.ImageRotate = 0F;
+            pbTime2.Location = new Point(118, 13);
+            pbTime2.Name = "pbTime2";
+            pbTime2.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            pbTime2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            pbTime2.Size = new Size(100, 90);
+            pbTime2.TabIndex = 0;
+            pbTime2.TabStop = false;
+            // 
+            // pbTime1
+            // 
+            pbTime1.FillColor = SystemColors.WindowFrame;
+            pbTime1.ImageRotate = 0F;
+            pbTime1.Location = new Point(12, 13);
+            pbTime1.Name = "pbTime1";
+            pbTime1.ShadowDecoration.CustomizableEdges = customizableEdges15;
+            pbTime1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            pbTime1.Size = new Size(100, 90);
+            pbTime1.TabIndex = 0;
+            pbTime1.TabStop = false;
+            // 
+            // pbUltimoPokemon
+            // 
+            pbUltimoPokemon.ImageRotate = 0F;
+            pbUltimoPokemon.Location = new Point(994, 3);
+            pbUltimoPokemon.Name = "pbUltimoPokemon";
+            pbUltimoPokemon.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            pbUltimoPokemon.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            pbUltimoPokemon.Size = new Size(128, 128);
+            pbUltimoPokemon.TabIndex = 3;
+            pbUltimoPokemon.TabStop = false;
+            // 
+            // lblUltimoPokemon
+            // 
+            lblUltimoPokemon.BackColor = Color.Transparent;
+            lblUltimoPokemon.Location = new Point(979, 137);
+            lblUltimoPokemon.Name = "lblUltimoPokemon";
+            lblUltimoPokemon.Size = new Size(152, 17);
+            lblUltimoPokemon.TabIndex = 4;
+            lblUltimoPokemon.Text = "Ultimo Pokemon Capturado";
+            // 
+            // lblPokemonCapturados
+            // 
+            lblPokemonCapturados.BackColor = Color.Transparent;
+            lblPokemonCapturados.Font = new Font("Segoe UI", 16F);
+            lblPokemonCapturados.Location = new Point(13, 122);
+            lblPokemonCapturados.Name = "lblPokemonCapturados";
+            lblPokemonCapturados.Size = new Size(288, 32);
+            lblPokemonCapturados.TabIndex = 5;
+            lblPokemonCapturados.Text = "Total de Pokemon Capturados:";
+            // 
+            // lblPcBox
+            // 
+            lblPcBox.BackColor = Color.Transparent;
+            lblPcBox.Font = new Font("Segoe UI", 16F);
+            lblPcBox.Location = new Point(13, 264);
+            lblPcBox.Name = "lblPcBox";
+            lblPcBox.Size = new Size(113, 32);
+            lblPcBox.TabIndex = 6;
+            lblPcBox.Text = "Seu PC Box:";
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Font = new Font("Segoe UI", 16F);
+            guna2HtmlLabel1.Location = new Point(13, 440);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(140, 32);
+            guna2HtmlLabel1.TabIndex = 7;
+            guna2HtmlLabel1.Text = "Seu time ativo:";
+            // 
+            // lblNomePokemon
+            // 
+            lblNomePokemon.BackColor = Color.Transparent;
+            lblNomePokemon.Font = new Font("Segoe UI", 12F);
+            lblNomePokemon.Location = new Point(1032, 160);
+            lblNomePokemon.Name = "lblNomePokemon";
+            lblNomePokemon.Size = new Size(46, 23);
+            lblNomePokemon.TabIndex = 7;
+            lblNomePokemon.Text = "Nome";
+            // 
+            // panelAdminStats
+            // 
+            panelAdminStats.Controls.Add(lblAdminNovosPokemons24h);
+            panelAdminStats.Controls.Add(lblAdminTotalPokemon);
+            panelAdminStats.Controls.Add(lblAdminNovosTreinadores24h);
+            panelAdminStats.Controls.Add(lblAdminTotalTreinadores);
+            panelAdminStats.CustomizableEdges = customizableEdges17;
+            panelAdminStats.Location = new Point(979, 214);
+            panelAdminStats.Name = "panelAdminStats";
+            panelAdminStats.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            panelAdminStats.Size = new Size(152, 258);
+            panelAdminStats.TabIndex = 8;
+            // 
+            // lblAdminTotalTreinadores
+            // 
+            lblAdminTotalTreinadores.BackColor = Color.Transparent;
+            lblAdminTotalTreinadores.Location = new Point(3, 3);
+            lblAdminTotalTreinadores.Name = "lblAdminTotalTreinadores";
+            lblAdminTotalTreinadores.Size = new Size(112, 17);
+            lblAdminTotalTreinadores.TabIndex = 0;
+            lblAdminTotalTreinadores.Text = "Total de Treinadores:";
+            // 
+            // lblAdminNovosTreinadores24h
+            // 
+            lblAdminNovosTreinadores24h.BackColor = Color.Transparent;
+            lblAdminNovosTreinadores24h.Location = new Point(3, 65);
+            lblAdminNovosTreinadores24h.Name = "lblAdminNovosTreinadores24h";
+            lblAdminNovosTreinadores24h.Size = new Size(104, 17);
+            lblAdminNovosTreinadores24h.TabIndex = 0;
+            lblAdminNovosTreinadores24h.Text = "Novos Treinadores:";
+            // 
+            // lblAdminTotalPokemon
+            // 
+            lblAdminTotalPokemon.BackColor = Color.Transparent;
+            lblAdminTotalPokemon.Location = new Point(3, 124);
+            lblAdminTotalPokemon.Name = "lblAdminTotalPokemon";
+            lblAdminTotalPokemon.Size = new Size(107, 17);
+            lblAdminTotalPokemon.TabIndex = 0;
+            lblAdminTotalPokemon.Text = "Total de Pokemons:";
+            // 
+            // lblAdminNovosPokemons24h
+            // 
+            lblAdminNovosPokemons24h.BackColor = Color.Transparent;
+            lblAdminNovosPokemons24h.Location = new Point(3, 186);
+            lblAdminNovosPokemons24h.Name = "lblAdminNovosPokemons24h";
+            lblAdminNovosPokemons24h.Size = new Size(99, 17);
+            lblAdminNovosPokemons24h.TabIndex = 0;
+            lblAdminNovosPokemons24h.Text = "Novos Pokemons:";
+            // 
+            // ucHome
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panelAdminStats);
+            Controls.Add(lblNomePokemon);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(lblPcBox);
+            Controls.Add(lblPokemonCapturados);
+            Controls.Add(lblUltimoPokemon);
+            Controls.Add(pbUltimoPokemon);
+            Controls.Add(panelTimeAtivo);
+            Controls.Add(lblDataCriacao);
+            Controls.Add(lblBoasVindas);
+            Name = "ucHome";
+            Size = new Size(1148, 650);
+            Load += ucHome_Load;
+            panelTimeAtivo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pbTime6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbTime1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbUltimoPokemon).EndInit();
+            panelAdminStats.ResumeLayout(false);
+            panelAdminStats.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblBoasVindas;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblDataCriacao;
+        private Guna.UI2.WinForms.Guna2ShadowPanel panelTimeAtivo;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbTime6;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbTime5;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbTime4;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbTime3;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbTime2;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbTime1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox pbUltimoPokemon;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblUltimoPokemon;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblPokemonCapturados;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblPcBox;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblNomePokemon;
+        private Guna.UI2.WinForms.Guna2Panel panelAdminStats;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblAdminNovosPokemons24h;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblAdminTotalPokemon;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblAdminNovosTreinadores24h;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblAdminTotalTreinadores;
+    }
+}
---
+++ PokeCore/ucHome.cs
@@ -0,0 +1,90 @@
+﻿using PokeCore.BLL;
+using PokeCore.DTO;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PokeCore.DesktopUI
+{
+    public partial class ucHome : UserControl
+    {
+        private TreinadorServiceBLL _bll;
+        private TreinadorDTO _treinadorLogado;
+
+        public ucHome(TreinadorDTO treinadorLogado)
+        {
+            InitializeComponent();
+            _treinadorLogado = treinadorLogado;
+            _bll = new TreinadorServiceBLL();
+
+            this.Load += ucHome_Load;
+        }
+
+        private void ucHome_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CarregarDadosDashboard()
+        {
+            try
+            {
+                lblBoasVindas.Text = $"Bem-vindo, " + _treinadorLogado.DisplayName + "!";
+                lblDataCriacao.Text = "Treinador desde: " + _treinadorLogado.CreatedAt.ToShortDateString;
+
+                int totalCapturados = _bll.GetTotalCapturedCount(_treinadorLogado.Id);
+                lblPokemonCapturados.Text = "Total de Pokemons Capturados: " + totalCapturados.ToString();
+
+                PokemonDTO ultimoPokemon = _bll.GetLastCapturedPokemon(_treinadorLogado.Id);
+                if (ultimoPokemon != null)
+                {
+                    lblNomePokemon.Text = ultimoPokemon.Nome;
+                }
+                else
+                {
+                    lblNomePokemon.Text = "Nenhum Pokemon Capturado";
+                }
+
+                List<PokemonDTO> timeAtivo = _bll.GetTreinadorActiveTeam(_treinadorLogado.Id);
+
+                var pictureBoxesTime = new List<Guna.UI2.WinForms.Guna2CirclePictureBox>
+            {
+                pbTime1, pbTime2, pbTime3, pbTime4, pbTime5, pbTime6
+            };
+
+                for (int i = 0; i < pictureBoxesTime.Count; i++)
+                {
+                    if (i < timeAtivo.Count)
+                    {
+                        PokemonDTO pokemon = timeAtivo[i];
+
+                        pictureBoxesTime[i].Visible = true;
+                        pictureBoxesTime[i].FillColor = SystemColors.WindowFrame;
+                    }
+                }
+
+                int pcCount = _bll.GetTreinadorPcBox(_treinadorLogado.Id).Count;
+                lblPcBox.Text = $"Seu PC Box: {pcCount} Pokémon";
+
+                if (_treinadorLogado.IsAdmin)
+                {
+                    panelAdminStats.Visible = true; // Mostra o painel de admin
+
+                    // Preenche os stats de Admin (você precisará criar esses Labels no designer)
+                    lblAdminTotalTreinadores.Text = _bll.AdminGetTotalTrainerCount(_treinadorLogado.Id).ToString();
+                    lblAdminTotalPokemon.Text = _bll.AdminGetTotalPokemonInSystem(_treinadorLogado.Id).ToString();
+                    lblAdminNovosPokemons24h.Text = _bll.AdminGetNewPokemonLast24h(_treinadorLogado.Id).ToString();
+                    lblAdminNovosTreinadores24h.Text = _bll.AdminGetNewTrainersLast24h(_treinadorLogado.Id).ToString();
+                }
+                else
+                {
+                    panelAdminStats.Visible = false; // Esconde o painel de admin
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar dados do dashboard: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
