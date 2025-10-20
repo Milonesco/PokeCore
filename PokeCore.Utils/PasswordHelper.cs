2d333c568cf796ea7eb6d24e8327453473322d7f
Milone <milonegabriel000@gmail.com>
2025-10-20 15:56:42 -0300

    feat(password): adiciona validação de força de senha
    Os métodos `HashPassword` e `VerifyPassword` foram comentados, removendo a funcionalidade de hash e verificação de senhas momentaneamente.
    
    Adicionados os métodos `ValidatePassword` e `ValidatePasswordStrength` para validar senhas.

--- PokeCore.Utils/PasswordHelper.cs
+++ PokeCore.Utils/PasswordHelper.cs
@@ -5,17 +5,49 @@ namespace PokeCore.Utils
 {
     public class PasswordHelper
     {
-        // Método para criar o hash
-        public static string HashPassword(string password)
+        //// Método para criar o hash
+        //public static string HashPassword(string password)
+        //{
+        //    // O "trabalho sujo" do BCrypt fica isolado aqui
+        //    return BCryptLib.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt(12));
+        //}
+
+        //// Método para verificar o hash
+        //public static bool VerifyPassword(string password, string passwordHash)
+        //{
+        //    return BCryptLib.Verify(password, passwordHash);
+        //}
+
+        public static void ValidatePassword(string password)
         {
-            // O "trabalho sujo" do BCrypt fica isolado aqui
-            return BCryptLib.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt(12));
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("A senha não pode ser vazia.");
+            }
         }
 
-        // Método para verificar o hash
-        public static bool VerifyPassword(string password, string passwordHash)
+        public static void ValidatePasswordStrength(string password)
         {
-            return BCryptLib.Verify(password, passwordHash);
+            if (password.Length < 8)
+            {
+                throw new ArgumentException("A senha deve ter pelo menos 8 caracteres.");
+            }
+            if (!password.Any(char.IsUpper))
+            {
+                throw new ArgumentException("A senha deve conter pelo menos uma letra maiúscula.");
+            }
+            if (!password.Any(char.IsLower))
+            {
+                throw new ArgumentException("A senha deve conter pelo menos uma letra minúscula.");
+            }
+            if (!password.Any(char.IsDigit))
+            {
+                throw new ArgumentException("A senha deve conter pelo menos um número.");
+            }
+            if (!password.Any(ch => !char.IsLetterOrDigit(ch)))
+            {
+                throw new ArgumentException("A senha deve conter pelo menos um caractere especial.");
+            }
         }
     }
 }
