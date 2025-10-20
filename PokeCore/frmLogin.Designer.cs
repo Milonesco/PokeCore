7d082f9d363adb4a0f1efd809b8ac0675a1d5820
Milone <milonegabriel000@gmail.com>
2025-10-20 16:01:31 -0300

    feat(usuario): adiciona tela de login com validação

---
+++ PokeCore/frmLogin.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace PokeCore.DesktopUI
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(components);
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            txtUsuario = new Guna.UI2.WinForms.Guna2TextBox();
+            txtSenha = new Guna.UI2.WinForms.Guna2TextBox();
+            btnEntrar = new Guna.UI2.WinForms.Guna2Button();
+            lblEsqueceuSenha = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lblCadastro = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel7 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel8 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(components);
+            panelLogin = new Guna.UI2.WinForms.Guna2Panel();
+            guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            mdAvisoFalhaLogin = new Guna.UI2.WinForms.Guna2MessageDialog();
+            panelLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            guna2BorderlessForm1.BorderRadius = 20;
+            guna2BorderlessForm1.ContainerControl = this;
+            guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Enabled = false;
+            guna2HtmlLabel1.Font = new Font("Segoe UI", 14F);
+            guna2HtmlLabel1.ForeColor = Color.Gray;
+            guna2HtmlLabel1.Location = new Point(152, 192);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(184, 27);
+            guna2HtmlLabel1.TabIndex = 0;
+            guna2HtmlLabel1.Text = "Bem vindo, treinador!";
+            // 
+            // guna2HtmlLabel2
+            // 
+            guna2HtmlLabel2.BackColor = Color.Transparent;
+            guna2HtmlLabel2.Enabled = false;
+            guna2HtmlLabel2.Font = new Font("Segoe UI", 24F, FontStyle.Bold);
+            guna2HtmlLabel2.ForeColor = Color.White;
+            guna2HtmlLabel2.Location = new Point(139, 150);
+            guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            guna2HtmlLabel2.Size = new Size(208, 47);
+            guna2HtmlLabel2.TabIndex = 1;
+            guna2HtmlLabel2.Text = "Acessar conta";
+            // 
+            // guna2HtmlLabel3
+            // 
+            guna2HtmlLabel3.BackColor = Color.Transparent;
+            guna2HtmlLabel3.Enabled = false;
+            guna2HtmlLabel3.Font = new Font("Segoe UI", 14F);
+            guna2HtmlLabel3.ForeColor = Color.White;
+            guna2HtmlLabel3.Location = new Point(27, 238);
+            guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            guna2HtmlLabel3.Size = new Size(132, 27);
+            guna2HtmlLabel3.TabIndex = 1;
+            guna2HtmlLabel3.Text = "Nome ou Email";
+            // 
+            // guna2HtmlLabel4
+            // 
+            guna2HtmlLabel4.BackColor = Color.Transparent;
+            guna2HtmlLabel4.Enabled = false;
+            guna2HtmlLabel4.Font = new Font("Segoe UI", 14F);
+            guna2HtmlLabel4.ForeColor = Color.White;
+            guna2HtmlLabel4.Location = new Point(29, 344);
+            guna2HtmlLabel4.Name = "guna2HtmlLabel4";
+            guna2HtmlLabel4.Size = new Size(55, 27);
+            guna2HtmlLabel4.TabIndex = 1;
+            guna2HtmlLabel4.Text = "Senha";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.BackColor = Color.Transparent;
+            txtUsuario.BorderColor = Color.Gray;
+            txtUsuario.BorderRadius = 10;
+            txtUsuario.CustomizableEdges = customizableEdges10;
+            txtUsuario.DefaultText = "";
+            txtUsuario.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtUsuario.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtUsuario.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtUsuario.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtUsuario.FillColor = Color.FromArgb(31, 32, 36);
+            txtUsuario.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtUsuario.Font = new Font("Segoe UI", 9F);
+            txtUsuario.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtUsuario.Location = new Point(27, 271);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.PlaceholderText = "Insira o seu nome ou email";
+            txtUsuario.SelectedText = "";
+            txtUsuario.ShadowDecoration.CustomizableEdges = customizableEdges11;
+            txtUsuario.Size = new Size(440, 36);
+            txtUsuario.TabIndex = 3;
+            // 
+            // txtSenha
+            // 
+            txtSenha.BackColor = Color.Transparent;
+            txtSenha.BorderColor = Color.Gray;
+            txtSenha.BorderRadius = 10;
+            txtSenha.CustomizableEdges = customizableEdges12;
+            txtSenha.DefaultText = "";
+            txtSenha.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtSenha.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtSenha.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtSenha.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtSenha.FillColor = Color.FromArgb(31, 32, 36);
+            txtSenha.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtSenha.Font = new Font("Segoe UI", 9F);
+            txtSenha.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtSenha.Location = new Point(27, 377);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PlaceholderText = "········";
+            txtSenha.SelectedText = "";
+            txtSenha.ShadowDecoration.CustomizableEdges = customizableEdges13;
+            txtSenha.Size = new Size(440, 36);
+            txtSenha.TabIndex = 3;
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.BackColor = Color.Transparent;
+            btnEntrar.BorderRadius = 10;
+            btnEntrar.CustomizableEdges = customizableEdges14;
+            btnEntrar.DisabledState.BorderColor = Color.DarkGray;
+            btnEntrar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnEntrar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnEntrar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnEntrar.FillColor = Color.FromArgb(221, 23, 58);
+            btnEntrar.Font = new Font("Segoe UI", 9F);
+            btnEntrar.ForeColor = Color.White;
+            btnEntrar.Location = new Point(27, 485);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.ShadowDecoration.CustomizableEdges = customizableEdges15;
+            btnEntrar.Size = new Size(440, 45);
+            btnEntrar.TabIndex = 4;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.Click += btnEntrar_Click;
+            // 
+            // lblEsqueceuSenha
+            // 
+            lblEsqueceuSenha.BackColor = Color.Transparent;
+            lblEsqueceuSenha.Font = new Font("Segoe UI", 12F);
+            lblEsqueceuSenha.Location = new Point(335, 419);
+            lblEsqueceuSenha.Name = "lblEsqueceuSenha";
+            lblEsqueceuSenha.Size = new Size(132, 23);
+            lblEsqueceuSenha.TabIndex = 0;
+            lblEsqueceuSenha.Text = "<a href=\"#\" class=\"text-sm text-[#DD173A] hover:underline\">Esqueceu a senha?</a>\r\n";
+            // 
+            // lblCadastro
+            // 
+            lblCadastro.BackColor = Color.Transparent;
+            lblCadastro.Font = new Font("Segoe UI", 12F);
+            lblCadastro.Location = new Point(137, 536);
+            lblCadastro.Name = "lblCadastro";
+            lblCadastro.Size = new Size(233, 70);
+            lblCadastro.TabIndex = 5;
+            lblCadastro.Text = "<p>Não tem uma conta? <a href=\"#\" class=\"font-medium text-[#DD173A] hover:underline\">Cadastre-se</a></p>";
+            lblCadastro.Click += lblCadastro_Click;
+            // 
+            // guna2HtmlLabel7
+            // 
+            guna2HtmlLabel7.BackColor = Color.Transparent;
+            guna2HtmlLabel7.Enabled = false;
+            guna2HtmlLabel7.Font = new Font("Segoe UI", 12F);
+            guna2HtmlLabel7.ForeColor = Color.White;
+            guna2HtmlLabel7.Location = new Point(202, 718);
+            guna2HtmlLabel7.Name = "guna2HtmlLabel7";
+            guna2HtmlLabel7.Size = new Size(336, 23);
+            guna2HtmlLabel7.TabIndex = 1;
+            guna2HtmlLabel7.Text = "© 2025 PokeCore. Todos os direitos reservados.";
+            // 
+            // guna2HtmlLabel8
+            // 
+            guna2HtmlLabel8.BackColor = Color.Transparent;
+            guna2HtmlLabel8.Enabled = false;
+            guna2HtmlLabel8.Font = new Font("Segoe UI", 12F);
+            guna2HtmlLabel8.ForeColor = Color.White;
+            guna2HtmlLabel8.Location = new Point(170, 689);
+            guna2HtmlLabel8.Name = "guna2HtmlLabel8";
+            guna2HtmlLabel8.Size = new Size(406, 23);
+            guna2HtmlLabel8.TabIndex = 1;
+            guna2HtmlLabel8.Text = "© 2025 Pokemon Company. Todos os direitos reservados.";
+            // 
+            // guna2Elipse1
+            // 
+            guna2Elipse1.BorderRadius = 0;
+            guna2Elipse1.TargetControl = panelLogin;
+            // 
+            // panelLogin
+            // 
+            panelLogin.BorderColor = Color.Gray;
+            panelLogin.BorderRadius = 30;
+            panelLogin.BorderThickness = 1;
+            panelLogin.Controls.Add(guna2CirclePictureBox1);
+            panelLogin.Controls.Add(lblCadastro);
+            panelLogin.Controls.Add(btnEntrar);
+            panelLogin.Controls.Add(guna2HtmlLabel1);
+            panelLogin.Controls.Add(txtSenha);
+            panelLogin.Controls.Add(lblEsqueceuSenha);
+            panelLogin.Controls.Add(txtUsuario);
+            panelLogin.Controls.Add(guna2HtmlLabel2);
+            panelLogin.Controls.Add(guna2HtmlLabel3);
+            panelLogin.Controls.Add(guna2HtmlLabel4);
+            panelLogin.CustomizableEdges = customizableEdges17;
+            panelLogin.FillColor = Color.FromArgb(58, 58, 58);
+            panelLogin.Location = new Point(109, 63);
+            panelLogin.Name = "panelLogin";
+            panelLogin.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            panelLogin.Size = new Size(500, 620);
+            panelLogin.TabIndex = 2;
+            // 
+            // guna2CirclePictureBox1
+            // 
+            guna2CirclePictureBox1.BackColor = Color.Transparent;
+            guna2CirclePictureBox1.Enabled = false;
+            guna2CirclePictureBox1.FillColor = SystemColors.WindowText;
+            guna2CirclePictureBox1.Image = Properties.Resources.poke_logo_colored;
+            guna2CirclePictureBox1.ImageRotate = 0F;
+            guna2CirclePictureBox1.Location = new Point(182, 16);
+            guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            guna2CirclePictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox1.Size = new Size(128, 128);
+            guna2CirclePictureBox1.TabIndex = 6;
+            guna2CirclePictureBox1.TabStop = false;
+            // 
+            // mdAvisoFalhaLogin
+            // 
+            mdAvisoFalhaLogin.Buttons = Guna.UI2.WinForms.MessageDialogButtons.OK;
+            mdAvisoFalhaLogin.Caption = "Erro para logar!";
+            mdAvisoFalhaLogin.Icon = Guna.UI2.WinForms.MessageDialogIcon.Error;
+            mdAvisoFalhaLogin.Parent = this;
+            mdAvisoFalhaLogin.Style = Guna.UI2.WinForms.MessageDialogStyle.Dark;
+            mdAvisoFalhaLogin.Text = null;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(31, 32, 36);
+            ClientSize = new Size(710, 760);
+            Controls.Add(panelLogin);
+            Controls.Add(guna2HtmlLabel8);
+            Controls.Add(guna2HtmlLabel7);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "frmLogin";
+            Text = "frmLogin";
+            panelLogin.ResumeLayout(false);
+            panelLogin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2Button btnEntrar;
+        private Guna.UI2.WinForms.Guna2TextBox txtSenha;
+        private Guna.UI2.WinForms.Guna2TextBox txtUsuario;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel4;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblEsqueceuSenha;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblCadastro;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel8;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel7;
+        private Guna.UI2.WinForms.Guna2Panel panelLogin;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2MessageDialog mdAvisoFalhaLogin;
+    }
+}
---
+++ PokeCore/frmLogin.cs
@@ -0,0 +1,53 @@
+﻿using PokeCore.BLL;
+using PokeCore.DTO;
+
+namespace PokeCore.DesktopUI
+{
+    public partial class frmLogin : Form
+    {
+        private TreinadorServiceBLL _bll;
+        public frmLogin()
+        {
+            InitializeComponent();
+            _bll = new TreinadorServiceBLL();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string identifier = txtUsuario.Text.Trim();
+                string senha = txtSenha.Text.Trim();
+
+                if (string.IsNullOrEmpty(identifier) || string.IsNullOrEmpty(senha))
+                {
+                    MessageBox.Show("Usuário e senha são obrigatórios.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                TreinadorDTO treinadorLogado = _bll.Login(identifier, senha);
+
+                frmMain mainForm = new frmMain(treinadorLogado);
+                mainForm.Show();
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                mdAvisoFalhaLogin.Show("Erro de Login", ex.Message);
+            }
+        }
+
+        private void lblCadastro_Click(object sender, EventArgs e)
+        {
+            using (frmCadastro formCadastro = new frmCadastro())
+            {
+                formCadastro.ShowDialog();
+            }
+        }
+    }
+}
