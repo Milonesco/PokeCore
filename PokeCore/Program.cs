a754d1dfec9f17e396e87d0b79e0649599dea16b
Milone <milonegabriel000@gmail.com>
2025-10-20 16:04:43 -0300

    feat(interface): altera formulário inicial para frmLogin

--- PokeCore/Program.cs
+++ PokeCore/Program.cs
@@ -1,17 +1,17 @@
+using PokeCore.BLL;
+using PokeCore.DesktopUI;
+using PokeCore.DTO;
+using PokeCore.Utils;
+
 namespace PokeCore
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new frmLogin());
         }
     }
 }
